1924488a064e1c461393ec032980ec626824da10
Paolo Falco <paolo.falcoapp@gmail.com>
2023-11-23 15:22:40 +0100

    Commit - add categories and relations

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,21 +1,97 @@
 
-INSERT INTO photos (title, description, image, visible) VALUES ('Beautiful Landscape', 'A breathtaking view of nature', 'https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', true);
-INSERT INTO photos (title, description, image, visible) VALUES ('City Lights', 'Captivating night cityscape', 'https://media.istockphoto.com/id/531625878/it/foto/times-square.jpg?s=612x612&w=0&k=20&c=DP91Gw2dVJZSGFMtO_Iquaw8QwTTP9reCZ_u-u6d0QM=', true);
-INSERT INTO photos (title, description, image, visible) VALUES ('Mysterious Forest', 'Explore the enchanted woods', 'https://images.locationscout.net/2020/03/mysterious-forest-czech-republic.jpg?h=1100&q=83', false);
-INSERT INTO photos (title, description, image, visible) VALUES ('Mountain Peak', 'A view from the top of the world', 'https://as1.ftcdn.net/v2/jpg/05/65/76/92/1000_F_565769257_yaHRp8Pts9SjlnSzHKNrQs264KoIsiwE.jpg', true);
-INSERT INTO photos (title, description, image, visible) VALUES ('Serene Lake', 'Relax by the calm and serene lake', 'https://www.shareoregon.com/system/images/239219/big/SereneLake.jpg?1501560016', true);
-INSERT INTO photos (title, description, image, visible) VALUES ('Snowy Mountains', 'Embrace the beauty of snowy mountain landscapes', 'https://lp-cms-production.imgix.net/2019-06/0fdc589a5fff00344442ec641cca14d6-snowy-mountains.jpg?auto=format&w=1920&h=640&fit=crop&crop=faces,edges&q=75', true);
-INSERT INTO photos (title, description, image, visible) VALUES ('Golden Fields', 'Fields of gold under the sunlight', 'https://static1.squarespace.com/static/51a39504e4b093105c265c24/t/58b25abfff7c508925d959d6/1573371781103/golden-fields-carranballac.jpg?format=1500w', true);
-INSERT INTO photos (title, description, image, visible) VALUES ('Cozy Cabin', 'Escape to a cozy cabin in the woods', 'https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/274524975.jpg?k=2d9c8225064ab727e611078da8c6fcce7b0473efafab45261dfaa27ac89e04be&o=', true);
-INSERT INTO photos (title, description, image, visible) VALUES ('Tropical Paradise', 'Discover the beauty of a tropical paradise', 'https://render.fineartamerica.com/images/rendered/default/poster/8/5/break/images/artworkimages/medium/2/3-tropical-paradise-rawpixel.jpg', true);
-INSERT INTO photos (title, description, image, visible) VALUES ('Desert Oasis', 'Find tranquility in the midst of a desert oasis', 'https://www.worldatlas.com/r/w1200/upload/e7/3c/42/shutterstock-1250207509.jpg', true);
-INSERT INTO photos (title, description, image, visible) VALUES ('Autumn Leaves', 'Experience the colors of autumn leaves', 'https://www.elitetreecare.com/wp-content/uploads/2019/10/autum-leaves.jpg', true);
-INSERT INTO photos (title, description, image, visible) VALUES ('Underwater World', 'Explore the wonders of the underwater world', 'https://static.vecteezy.com/system/resources/previews/023/847/397/large_2x/wonderful-and-beautiful-underwater-world-with-corals-and-tropical-fish-with-ai-generated-free-photo.jpg', true);
-INSERT INTO photos (title, description, image, visible) VALUES ('Ancient Ruins', 'Journey through ancient ruins and history', 'https://cdn-imgix.headout.com/microbrands-banner-image/image/73648cebac314a43970ed9e6de097aaf-AdobeStock_40207802.jpeg', true);
-INSERT INTO photos (title, description, image, visible) VALUES ('Rolling Hills', 'Rolling hills as far as the eye can see', 'https://www.kenduncan.com/wp-content/uploads/1970/01/VX1404.jpg', true);
-INSERT INTO photos (title, description, image, visible) VALUES ('Starlit Sky', 'Gaze at the stars in a clear night sky', 'https://www.storizen.com/wp-content/uploads/2020/07/starlit-sky-min.jpg', true);
-INSERT INTO photos (title, description, image, visible) VALUES ('Whitewater Rapids', 'Feel the adrenaline of whitewater rapids', 'https://mild2wildrafting.com/wp-content/uploads/2020/10/Upper-A.jpg', true);
-INSERT INTO photos (title, description, image, visible) VALUES ('Cherry Blossoms', 'Enjoy the beauty of cherry blossoms in spring', 'https://hips.hearstapps.com/hmg-prod/images/fuji-and-sakura-royalty-free-image-144483163-1562593125.jpg?crop=0.668xw:1.00xh;0,0&resize=2048:*', true);
-INSERT INTO photos (title, description, image, visible) VALUES ('Sunny Meadow', 'Relax in a sunny meadow surrounded by flowers', 'https://images.fineartamerica.com/images-medium-large-5/sunny-meadow-pobytov.jpg', true);
-INSERT INTO photos (title, description, image, visible) VALUES ('Dramatic Cliffs', 'Witness the drama of cliffs against the sea', 'https://149808162.v2.pressablecdn.com/wp-content/uploads/2012/07/3-Cliffs-of-Moher.jpg', true);
-INSERT INTO photos (title, description, image, visible) VALUES ('Frozen Lake', 'Experience the tranquility of a frozen lake', 'https://media.cntraveler.com/photos/5678fa4567dc82253d9f1c76/master/pass/frozen-abraham-lake-canada-cr-getty.jpg', true);
+INSERT INTO photos (created_at, title, description, image, visible) VALUES ('2023-11-23 14:50:00','Beautiful Landscape', 'Immerse yourself in the breathtaking beauty of nature with this captivating image. From lush green landscapes to serene water bodies, this photo captures the essence of the great outdoors. Let your imagination wander as you explore the intricate details of this picturesque scene.', 'https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', true);
+INSERT INTO photos (created_at, title, description, image, visible) VALUES ('2023-11-23 14:50:00', 'City Lights', 'Captivating night cityscape', 'https://media.istockphoto.com/id/531625878/it/foto/times-square.jpg?s=612x612&w=0&k=20&c=DP91Gw2dVJZSGFMtO_Iquaw8QwTTP9reCZ_u-u6d0QM=', true);
+INSERT INTO photos (created_at, title, description, image, visible) VALUES ('2023-11-23 14:50:00', 'Mysterious Forest', 'Explore the enchanted woods', 'https://images.locationscout.net/2020/03/mysterious-forest-czech-republic.jpg?h=1100&q=83', false);
+INSERT INTO photos (created_at, title, description, image, visible) VALUES ('2023-11-23 14:50:00', 'Mountain Peak', 'A view from the top of the world', 'https://as1.ftcdn.net/v2/jpg/05/65/76/92/1000_F_565769257_yaHRp8Pts9SjlnSzHKNrQs264KoIsiwE.jpg', true);
+INSERT INTO photos (created_at, title, description, image, visible) VALUES ('2023-11-23 14:50:00', 'Serene Lake', 'Relax by the calm and serene lake', 'https://www.shareoregon.com/system/images/239219/big/SereneLake.jpg?1501560016', true);
+INSERT INTO photos (created_at, title, description, image, visible) VALUES ('2023-11-23 14:50:00', 'Snowy Mountains', 'Embrace the beauty of snowy mountain landscapes', 'https://lp-cms-production.imgix.net/2019-06/0fdc589a5fff00344442ec641cca14d6-snowy-mountains.jpg?auto=format&w=1920&h=640&fit=crop&crop=faces,edges&q=75', true);
+INSERT INTO photos (created_at, title, description, image, visible) VALUES ('2023-11-23 14:50:00', 'Golden Fields', 'Fields of gold under the sunlight', 'https://static1.squarespace.com/static/51a39504e4b093105c265c24/t/58b25abfff7c508925d959d6/1573371781103/golden-fields-carranballac.jpg?format=1500w', true);
+INSERT INTO photos (created_at, title, description, image, visible) VALUES ('2023-11-23 14:50:00', 'Cozy Cabin', 'Escape to a cozy cabin in the woods', 'https://q-xx.bstatic.com/xdata/images/hotel/max1024x768/274524975.jpg?k=2d9c8225064ab727e611078da8c6fcce7b0473efafab45261dfaa27ac89e04be&o=', true);
+INSERT INTO photos (created_at, title, description, image, visible) VALUES ('2023-11-23 14:50:00', 'Tropical Paradise', 'Discover the beauty of a tropical paradise', 'https://render.fineartamerica.com/images/rendered/default/poster/8/5/break/images/artworkimages/medium/2/3-tropical-paradise-rawpixel.jpg', true);
+INSERT INTO photos (created_at, title, description, image, visible) VALUES ('2023-11-23 14:50:00', 'Desert Oasis', 'Find tranquility in the midst of a desert oasis', 'https://www.worldatlas.com/r/w1200/upload/e7/3c/42/shutterstock-1250207509.jpg', true);
+INSERT INTO photos (created_at, title, description, image, visible) VALUES ('2023-11-23 14:50:00', 'Autumn Leaves', 'Experience the colors of autumn leaves', 'https://www.elitetreecare.com/wp-content/uploads/2019/10/autum-leaves.jpg', true);
+INSERT INTO photos (created_at, title, description, image, visible) VALUES ('2023-11-23 14:50:00', 'Underwater World', 'Explore the wonders of the underwater world', 'https://static.vecteezy.com/system/resources/previews/023/847/397/large_2x/wonderful-and-beautiful-underwater-world-with-corals-and-tropical-fish-with-ai-generated-free-photo.jpg', true);
+INSERT INTO photos (created_at, title, description, image, visible) VALUES ('2023-11-23 14:50:00', 'Ancient Ruins', 'Journey through ancient ruins and history', 'https://cdn-imgix.headout.com/microbrands-banner-image/image/73648cebac314a43970ed9e6de097aaf-AdobeStock_40207802.jpeg', true);
+INSERT INTO photos (created_at, title, description, image, visible) VALUES ('2023-11-23 14:50:00', 'Rolling Hills', 'Rolling hills as far as the eye can see', 'https://www.kenduncan.com/wp-content/uploads/1970/01/VX1404.jpg', true);
+INSERT INTO photos (created_at, title, description, image, visible) VALUES ('2023-11-23 14:50:00', 'Starlit Sky', 'Gaze at the stars in a clear night sky', 'https://www.storizen.com/wp-content/uploads/2020/07/starlit-sky-min.jpg', true);
+INSERT INTO photos (created_at, title, description, image, visible) VALUES ('2023-11-23 14:50:00', 'Whitewater Rapids', 'Feel the adrenaline of whitewater rapids', 'https://mild2wildrafting.com/wp-content/uploads/2020/10/Upper-A.jpg', true);
+INSERT INTO photos (created_at, title, description, image, visible) VALUES ('2023-11-23 14:50:00', 'Cherry Blossoms', 'Enjoy the beauty of cherry blossoms in spring', 'https://hips.hearstapps.com/hmg-prod/images/fuji-and-sakura-royalty-free-image-144483163-1562593125.jpg?crop=0.668xw:1.00xh;0,0&resize=2048:*', true);
+INSERT INTO photos (created_at, title, description, image, visible) VALUES ('2023-11-23 14:50:00', 'Sunny Meadow', 'Relax in a sunny meadow surrounded by flowers', 'https://images.fineartamerica.com/images-medium-large-5/sunny-meadow-pobytov.jpg', true);
+INSERT INTO photos (created_at, title, description, image, visible) VALUES ('2023-11-23 14:50:00', 'Dramatic Cliffs', 'Witness the drama of cliffs against the sea', 'https://149808162.v2.pressablecdn.com/wp-content/uploads/2012/07/3-Cliffs-of-Moher.jpg', true);
+INSERT INTO photos (created_at, title, description, image, visible) VALUES ('2023-11-23 14:50:00', 'Frozen Lake', 'Experience the tranquility of a frozen lake', 'https://media.cntraveler.com/photos/5678fa4567dc82253d9f1c76/master/pass/frozen-abraham-lake-canada-cr-getty.jpg', true);
+
+INSERT INTO categories (name) VALUES ('Nature');
+INSERT INTO categories (name) VALUES ('Landscapes');
+INSERT INTO categories (name) VALUES ('Wildlife');
+INSERT INTO categories (name) VALUES ('Cityscapes');
+INSERT INTO categories (name) VALUES ('Abstract');
+INSERT INTO categories (name) VALUES ('Travel');
+INSERT INTO categories (name) VALUES ('Architecture');
+INSERT INTO categories (name) VALUES ('People');
+INSERT INTO categories (name) VALUES ('Food');
+INSERT INTO categories (name) VALUES ('Technology');
+INSERT INTO categories (name) VALUES ('Sports');
+INSERT INTO categories (name) VALUES ('Fashion');
+INSERT INTO categories (name) VALUES ('Music');
+INSERT INTO categories (name) VALUES ('Animals');
+INSERT INTO categories (name) VALUES ('Black and White');
+
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (1, 2);
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (1, 3);
+
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (2, 5);
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (2, 6);
+
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (3, 7);
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (3, 8);
+
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (4, 9);
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (4, 10);
+
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (5, 11);
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (5, 12);
+
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (6, 13);
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (6, 14);
+
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (7, 15);
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (7, 16);
+
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (8, 17);
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (8, 18);
+
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (9, 19);
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (9, 20);
+
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (10, 1);
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (10, 2);
+
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (11, 3);
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (11, 4);
+
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (12, 5);
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (12, 6);
+
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (13, 7);
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (13, 8);
+
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (14, 9);
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (14, 10);
+
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (15, 11);
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (15, 12);
+
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (16, 13);
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (16, 14);
+
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (17, 15);
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (17, 16);
+
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (18, 17);
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (18, 18);
+
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (19, 19);
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (19, 20);
+
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (20, 1);
+INSERT INTO photos_categories (photo_id, categories_id) VALUES (20, 2);
