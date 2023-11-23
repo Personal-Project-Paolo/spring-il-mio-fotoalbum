5e7f34ffe4c469d1fd02e1deae8dd6274124e9b4
Paolo Falco <paolo.falcoapp@gmail.com>
2023-11-23 10:58:34 +0100

    Commit - add Photo service with index

---
+++ src/main/java/org/learning/springilmiofotoalbum/service/PhotoService.java
@@ -0,0 +1,26 @@
+package org.learning.springilmiofotoalbum.service;
+
+import org.learning.springilmiofotoalbum.model.Photo;
+import org.learning.springilmiofotoalbum.repository.PhotoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PhotoService {
+
+    @Autowired
+    private PhotoRepository photoRepository;
+
+    //Index
+    public List<Photo> getPhotoList(Optional<String> search){
+      if(search.isPresent()){
+          return photoRepository.findByNameContainingIgnoreCase(search.get());
+      }
+      else {
+          return photoRepository.findAll();
+      }
+    }
+}
