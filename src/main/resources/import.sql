1eea1a8355a6f3170da817d5779ef184a0cdc6f3
Paolo Falco <paolo.falcoapp@gmail.com>
2023-11-23 12:01:35 +0100

    Commit - add photos

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO photos (title, description, image, visible) VALUES ('Beautiful Landscape', 'A breathtaking view of nature', 'https://example.com/landscape.jpg', true);
+INSERT INTO photos (title, description, image, visible) VALUES ('City Lights', 'Captivating night cityscape', 'https://example.com/citylights.jpg', true);
+INSERT INTO photos (title, description, image, visible) VALUES ('Mysterious Forest', 'Explore the enchanted woods', 'https://example.com/forest.jpg', false);
