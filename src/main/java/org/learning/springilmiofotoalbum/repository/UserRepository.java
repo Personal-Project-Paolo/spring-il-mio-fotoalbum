09849b29054780c12288cf111cc619a790672edf
Paolo Falco <paolo.falcoapp@gmail.com>
2023-11-24 15:24:54 +0100

    Commit - add UserRepository

---
+++ src/main/java/org/learning/springilmiofotoalbum/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package org.learning.springilmiofotoalbum.repository;
+
+import org.learning.springilmiofotoalbum.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
