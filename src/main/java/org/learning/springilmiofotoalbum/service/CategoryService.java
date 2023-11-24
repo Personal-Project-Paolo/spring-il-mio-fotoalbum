e6c4eb1df4860ab8b0a9d51a3f6f5837c1bb5354
Paolo Falco <paolo.falcoapp@gmail.com>
2023-11-24 09:52:23 +0100

    Commit - add CategoryService with index

---
+++ src/main/java/org/learning/springilmiofotoalbum/service/CategoryService.java
@@ -0,0 +1,19 @@
+package org.learning.springilmiofotoalbum.service;
+
+import org.learning.springilmiofotoalbum.model.Category;
+import org.learning.springilmiofotoalbum.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    public List<Category> getAll() {
+        return categoryRepository.findByOrderByName();
+    }
+}
