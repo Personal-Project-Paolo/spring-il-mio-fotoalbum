cc24d0f884fb26b814e2b42adb0cc0b595f82dfc
Paolo Falco <paolo.falcoapp@gmail.com>
2023-11-24 15:25:34 +0100

    Commit - add DatabaseUserDetailsService

---
+++ src/main/java/org/learning/springilmiofotoalbum/security/DatabaseUserDetailsService.java
@@ -0,0 +1,28 @@
+package org.learning.springilmiofotoalbum.security;
+
+import org.learning.springilmiofotoalbum.model.User;
+import org.learning.springilmiofotoalbum.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class DatabaseUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> loggedUser = userRepository.findByEmail(username);
+        if (loggedUser.isPresent()){
+            return new DatabaseUserDetails(loggedUser.get());
+        }else {
+            throw new UsernameNotFoundException(username);
+        }
+    }
+}
