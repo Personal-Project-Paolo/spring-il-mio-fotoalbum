94e45164d8317791de64eee89b8066cceb623f07
Paolo Falco <paolo.falcoapp@gmail.com>
2023-11-25 23:01:00 +0100

    Commit - add Create and contact - frontend

---
+++ front-end/create.js
@@ -0,0 +1,27 @@
+const apiMessages = 'http://localhost:8080/api/v1/messages';
+
+// funzione per creare un nuovo messaggio
+
+document.getElementById('messageForm').addEventListener('submit', async function(event){
+    event.preventDefault();
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    try{
+        await axios.post(apiMessages, {
+            email,
+            message,
+        });
+
+        // Imposta il messaggio di successo nell'HTML
+        successMessage.style.display = 'block';
+        this.style.display = 'none';
+
+        setTimeout(() => {
+            window.location.href = "contact.html";
+        }, 3000); 
+
+    }catch(error){
+        console.error(error);
+    }
+})
+
