6d66d643d6b8c6a4239cca9bdc2f1fa177d22158
Paolo Falco <paolo.falcoapp@gmail.com>
2023-11-25 15:31:22 +0100

    Commit - add MessageRepository

---
+++ src/main/java/org/learning/springilmiofotoalbum/repository/MessageRepository.java
@@ -0,0 +1,8 @@
+package org.learning.springilmiofotoalbum.repository;
+
+
+import org.learning.springilmiofotoalbum.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+}
