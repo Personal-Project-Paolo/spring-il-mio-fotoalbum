26b717c60ea76966bdc700d7a518b6ffcc055d13
Paolo Falco <paolo.falcoapp@gmail.com>
2023-11-28 12:07:35 +0100

    Commit - add method for relation

--- src/main/java/org/learning/springilmiofotoalbum/repository/PhotoRepository.java
+++ src/main/java/org/learning/springilmiofotoalbum/repository/PhotoRepository.java
@@ -1,13 +1,23 @@
 package org.learning.springilmiofotoalbum.repository;
 
 import org.learning.springilmiofotoalbum.model.Photo;
+import org.learning.springilmiofotoalbum.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface PhotoRepository extends JpaRepository<Photo, Integer> {
+
+    //Backend
     List<Photo> findByTitleContainingIgnoreCase(String titleKeyword);
+    List<Photo> findByUserIdAndTitleContainingIgnoreCase(Integer userId, String titleKeyword);
+    List<Photo> findByUserId(Integer userId);
+
+    //Frontend
     List<Photo> findByVisible(boolean visible);
     List<Photo> findByVisibleAndTitleContainingIgnoreCase(boolean visible, String titleKeyword);
-
+    List<Photo> findByUserIdAndVisible(Integer userId, boolean visible);
+    List<Photo> findByUserIdAndVisibleAndTitleContainingIgnoreCase(
+            Integer userId, boolean visible, String titleKeyword
+    );
 }
